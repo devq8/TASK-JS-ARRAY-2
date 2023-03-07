520f32925840e694ddf2b3c111a07df04c8919af
Khaled Alghanim <al.ghanim@me.com>
2023-03-07 18:46:04 +0300

    Done

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
 // 1. Declare an empty array called `myArray`.
 let myArray = []
+console.log(myArray)
 
 // 2. Create an array called 'fruits' that contains the following elements: 'apple', 'banana', 'cherry'.
 let fruits = ['apple', 'banana', 'cherry']
+console.log(fruits)
 
 // 3. Access the second element of the fruits array and assign it to a variable called 'secondFruit'.
-let secondFruit = fruits[2]
+let secondFruit = fruits[1]
+console.log(secondFruit)
 
 // 4. Add the element 'orange' to the end of the 'fruits' array.
 fruits.push('orange')
+console.log(fruits)
 
 // 5. Remove the first element from the 'fruits' array.
 fruits.shift()
+console.log(fruits)
 
 // 6. Find the length of the 'fruits' array and assign it to a variable called 'numFruits'.
 let numFruits = fruits.length
+console.log(numFruits)
 
 // 7. Check if the element 'banana' exists in the 'fruits' array and assign the result to a variable called 'hasBanana'.
 let hasBanana = fruits.includes('banana')
+console.log(hasBanana)
 
 // 8. Loop through the 'fruits' array and console.log each element.
 fruits.forEach(x => console.log(x))
 
 // 9. Create a new array called 'numbers' that contains the numbers 1, 2, 3, 4.
 let numbers = [1,2,3,4]
+console.log(numbers)
 
 // 10 Use the 'map' method to create a new array called 'squares' that contains the squares of each number in the 'numbers' array.
 numbers = numbers.map(x => x*x)
